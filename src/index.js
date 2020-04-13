4115fc62d0504bff3f2ec574ee2aad625afa257b
Abdulazeez Abdulazeez Adeshina <laisibizness@gmail.com>
2020-04-13 03:35:24 +0100

    Render TodoLis5

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import React from "react";
 import { render } from "react-dom";
+import TodoList from "./Components/Todos";
+
+function App() {
+  return (
+    <TodoList />
+  )
+}
+
+render(<App />, document.getElementById("root"))
